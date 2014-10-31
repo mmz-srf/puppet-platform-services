6d28a48d32793507ee60c0e3475d31bab004ed3b
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2014-10-31 13:36:01 +0100

    make guest_vm_ip optional

--- platform_services_cloudstack/manifests/port_forwarding.pp
+++ platform_services_cloudstack/manifests/port_forwarding.pp
@@ -6,8 +6,18 @@ define platform_services_cloudstack::port_forwarding(
 ) {
   debug("${caller_module_name}->${module_name} : Configuring Portforwarding with front_id:${front_ip}, protocol:${protocol}, port:${port}")
 
-  if $::platform_services::manage_front_ips {
-    @@cloudstack_port_forwarding{"$::fqdn/$protocol/$name":
+  if $::platform_services::manage_front_ips and defined("$vm_guest_ip") {
+    @@cloudstack_port_forwarding {"$::fqdn/$protocol/$name":
+      ensure             => present,
+      front_ip           => $front_ip,
+      protocol           => $protocol,
+      privateport        => $port,
+      publicport         => $port,
+      virtual_machine_id => $::instance_id,
+      vm_guest_ip        => $vm_guest_ip,
+    }
+  }elsif $::platform_services::manage_front_ips {
+    @@cloudstack_port_forwarding {"$::fqdn/$protocol/$name":
       ensure             => present,
       front_ip           => $front_ip,
       protocol           => $protocol,
