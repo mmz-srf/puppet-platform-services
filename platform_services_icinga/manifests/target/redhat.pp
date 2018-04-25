8fb1e8114acff891f95b39a0e1d33ba41bbc02a9
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2018-04-25 08:06:45 +0200

    add icinga note url for centos VMs
    AIS-18630

--- platform_services_icinga/manifests/target/redhat.pp
+++ platform_services_icinga/manifests/target/redhat.pp
@@ -4,9 +4,11 @@ class platform_services_icinga::target::redhat (
   package { 'yum-plugin-security':
     ensure => installed,
   }
+  $icinga_notes_url = hiera('icinga_notes_url', 'http://localhost')
   icinga::service { 'yum':
     service_description => 'Check for updates',
     check_command       => 'check_yum',
     ensure              => $yum_check,
+    notes_url           => "${icinga_notes_url}DYB8Iw",
   }
 }
