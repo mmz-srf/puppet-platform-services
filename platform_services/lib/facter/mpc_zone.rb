80edb1f480f3744e57cb8f10573ff0776346827e
Claudio Walser <claudio.walser@srf.ch>
2019-01-11 10:35:31 +0100

    puppet4 compatible

--- platform_services/lib/facter/mpc_zone.rb
+++ platform_services/lib/facter/mpc_zone.rb
@@ -1,6 +1,6 @@
 Facter.add("mpc_zone") do
   setcode do
-    ip = Facter.value('ipaddress_eth0')
+    ip = Facter.value('network_primary_ip')
     case ip[/^\d+\.(\d+)\.\d+\.\d+$/, 1]
     when '101'
       'bie'
