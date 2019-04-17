800a5eaee33899af824eb43d78cbbb9ed89e4bbc
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2019-04-17 07:22:31 +0200

    get ready for debian 9 upgrade
    AIS-20617

--- platform_services_haproxy/manifests/server.pp
+++ platform_services_haproxy/manifests/server.pp
@@ -1,13 +1,14 @@
 class platform_services_haproxy::server(
   $high_available = false,
-  $site_classes = undef
+  $site_classes   = undef
 ) {
   class{'::haproxy':}
   if $high_available {
     sysctl::value{'net.ipv4.ip_nonlocal_bind': value => '1'}
     include keepalived
-    keepalived::vrrp_script{'haproxy': 
-      name_is_process => true,  
+    ensure_resource('package', 'libipset3', {ensure => installed})
+    keepalived::vrrp_script{'haproxy':
+      script => '/usr/bin/killall -0 haproxy', 
       weight => 51,
     }
   }
