4881c903d65d528f3b8da5f4f9f695fba634b86f
Claudio Walser <claudio.walser@srf.ch>
2019-06-06 10:42:24 +0200

    get ready for debian buster

--- platform_services_haproxy/manifests/server.pp
+++ platform_services_haproxy/manifests/server.pp
@@ -1,12 +1,18 @@
 class platform_services_haproxy::server(
   $high_available = false,
-  $site_classes   = undef
+  $site_classes   = undef,
 ) {
   class{'::haproxy':}
   if $high_available {
     sysctl::value{'net.ipv4.ip_nonlocal_bind': value => '1'}
     include keepalived
-    ensure_resource('package', 'libipset3', {ensure => installed})
+
+    $libipset_package = 'libipset3'
+    if $::lsbdistcodename == 'buster' {
+      $libipset_package = 'libipset11'
+    }
+    ensure_resource('package', $libipset_package, {ensure => installed})
+
     keepalived::vrrp_script{'haproxy':
       script => '/usr/bin/killall -0 haproxy', 
       weight => 51,
