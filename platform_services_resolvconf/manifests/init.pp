6bcfb027137a6019937ef3bfa9a688e86a9f6a08
mms-gianni <gianni.carafa@srf.ch>
2020-01-07 15:15:56 +0100

    do noting, if running in docker VM

--- platform_services_resolvconf/manifests/init.pp
+++ platform_services_resolvconf/manifests/init.pp
@@ -1,26 +1,29 @@
 class platform_services_resolvconf(
     $timeout = 3,
 ){
-  resolvconf::search{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
-  resolvconf::search{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
+  if $::virtual != 'docker' {
+    resolvconf::search{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
+    resolvconf::search{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
 
-  resolvconf::option {'timeout':
-    value => $timeout,
-  }
+    resolvconf::option {'timeout':
+      value => $timeout,
+    }
 
-  if $::mpc_network_front == $::network_primary_ip {
-    resolvconf::domain{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
-    Resolvconf::Nameserver <<|tag=='front'|>>
-  } else {
-    resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
-    Resolvconf::Nameserver <<|tag=='internal'|>>
-    $default_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
-    if $default_nameserver != $::ipaddress and
-       !defined(Resolvconf::Nameserver[$default_nameserver]) {
-      resolvconf::nameserver{$default_nameserver:
-        priority => 10,
-        tag => 'internal',
+    if $::mpc_network_front == $::network_primary_ip {
+      resolvconf::domain{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
+      Resolvconf::Nameserver <<|tag=='front'|>>
+    } else {
+      resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
+      Resolvconf::Nameserver <<|tag=='internal'|>>
+      $default_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
+      if $default_nameserver != $::ipaddress and
+        !defined(Resolvconf::Nameserver[$default_nameserver]) {
+        resolvconf::nameserver{$default_nameserver:
+          priority => 10,
+          tag => 'internal',
+        }
       }
     }
+
   }
 }
