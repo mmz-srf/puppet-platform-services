60ec30952eda20099305b898fc43266fb668690c
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2020-08-14 13:54:54 +0200

    added cidrlist

--- platform_services_cloudstack/manifests/port_forwarding.pp
+++ platform_services_cloudstack/manifests/port_forwarding.pp
@@ -1,7 +1,8 @@
 define platform_services_cloudstack::port_forwarding(
   $front_ip,
-  $protocol = 'tcp',
-  $port = $name,
+  $protocol    = 'tcp',
+  $port        = $name,
+  $cidrlist    = "0.0.0.0/0"
   $vm_guest_ip = "0.0.0.0",
 ) {
   debug("${caller_module_name}->${module_name} : Configuring Portforwarding with front_id:${front_ip}, protocol:${protocol}, port:${port}")
@@ -14,6 +15,7 @@ define platform_services_cloudstack::port_forwarding(
       privateport        => $port,
       publicport         => $port,
       virtual_machine_id => $::instance_id,
+      cidrlist           => $cidrlist,
       vm_guest_ip        => $vm_guest_ip,
     }
   }
