f7c521c9bd53bc9e6d20069f88760f0280095b71
Bruno Mazenauer <bruno.mazenauer@srf.ch>
2020-08-19 10:15:29 +0200

    introduce cidrlist
    PLATFORM-635

--- platform_services_cloudstack/manifests/port_forwarding.pp
+++ platform_services_cloudstack/manifests/port_forwarding.pp
@@ -1,13 +1,24 @@
-define platform_services_cloudstack::port_forwarding(
+define platform_services_cloudstack::port_forwarding (
   $front_ip,
-  $protocol    = 'tcp',
   $port        = $name,
-  $cidrlist    = "0.0.0.0/0",
+  $protocol    = 'tcp',
   $vm_guest_ip = "0.0.0.0",
+  $cidrlist    = '0.0.0.0/0',
 ) {
-  debug("${caller_module_name}->${module_name} : Configuring Portforwarding with front_id:${front_ip}, protocol:${protocol}, port:${port}")
 
   if $::platform_services::manage_front_ips {
+    debug("${caller_module_name}->${module_name} : Configuring Firewallrule for ${front_ip} [source cidr:${cidrlist}, start port:${port}, end port:${port}, protocol:${protocol}]")
+    @@cloudstack_firewall_rule {"$::fqdn/$cidrlist/$port/$protocol/$name":
+      ensure             => present,
+      front_ip           => $front_ip,
+      cidrlist           => $cidrlist,
+      startport          => $port,
+      endport            => $port,
+      protocol           => $protocol,
+      virtual_machine_id => $::instance_id,
+    }
+
+    debug("${caller_module_name}->${module_name} : Configuring Portforwarding with front_id:${front_ip}, protocol:${protocol}, port:${port}")
     @@cloudstack_port_forwarding {"$::fqdn/$vm_guest_ip/$protocol/$name":
       ensure             => present,
       front_ip           => $front_ip,
@@ -19,4 +30,5 @@ define platform_services_cloudstack::port_forwarding(
       vm_guest_ip        => $vm_guest_ip,
     }
   }
+
 }
