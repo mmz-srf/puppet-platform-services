a35261f871ae9d0d82e4ce08fbf1133541e85d61
srf-stefan <srf-stefan@users.noreply.github.com>
2020-11-25 14:31:40 +0100

    make sure to use puppet apr v4 conform function
    PLATFORM-873

--- platform_services_dns/manifests/server/zone.pp
+++ platform_services_dns/manifests/server/zone.pp
@@ -10,7 +10,7 @@ define platform_services_dns::server::zone(
     zone_ttl => 60,
     zone_minimum => 60,
   }
-  $rdns_zones = ip_to_arpa($rdns_networks, $::platform_services::networks_netmask)
+  $rdns_zones = srf_ip_to_arpa($rdns_networks, $::platform_services::networks_netmask)
   dns::zone{$rdns_zones:
     soa => "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
     soa_email => "admin.$name",
